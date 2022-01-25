0926f663478adf2a208e9fcc3a4ceeb792288b26
桑晓龙 <sangxl@iict.ac.cn>
2022-01-25 13:06:28 +0800

    修改目录权限前增加判断

--- run.sh
+++ run.sh
@@ -3,9 +3,15 @@
 . ./.env
 
 # create log files
-mkdir -p logs/besu logs/tessera
-chmod -R 0777 logs
-chmod -R 0777 data
+if [ ! -d logs ]; then
+    mkdir -p logs/besu logs/tessera
+    chmod -R 0777 logs
+fi
+
+if [ ! -d data ]; then
+    mkdir data
+    chmod -R 0777 data
+fi
 
 #echo "Starting Member Peer......"
 docker-compose up -d
